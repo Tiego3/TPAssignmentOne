a62795861971c3788b67089e4fe01aec615a61b3
Tiego3 <tiego3m@gmail.com>
2016-03-07 00:52:22 +0200

    aa

--- src/main/java/za/ac/cput/TPAssignmentOne/AssignmentOne.java
+++ src/main/java/za/ac/cput/TPAssignmentOne/AssignmentOne.java
@@ -4,4 +4,9 @@ package za.ac.cput.TPAssignmentOne;
  * Created by student on 2016/03/07.
  */
 public class AssignmentOne {
+
+    public String Name()
+    {
+        return "AssignmentOne";
+    }
 }
